f6c4a461daa11ab4dde374c027c15cb635136f45
mischievousdev <adevhubyt@gmail.com>
2020-02-14 19:25:03 +0530

    fixed

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	author_email="miscdev.py@gmail.com",
 	version="1.0.0",
 	url="https://github.com/Pythonastics/pymail/tree/master"
-	packages = ["pymail"],
+	packages=["pymail"],
 	license="MIT",
 	description="An API Wrapper of gmail to send instant mails using python",
 	long_description=rme,
