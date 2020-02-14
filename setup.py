733ccbfb7bf79577cc930c197696fff61e0a5798
mischievousdev <adevhubyt@gmail.com>
2020-02-14 19:20:27 +0530

    fixed typo

--- setup.py
+++ setup.py
@@ -13,20 +13,5 @@ setup(
 	license="MIT",
 	description="An API Wrapper of gmail to send instant mails using python",
 	long_description=rme,
-	keywords='gmail email wrapper',
-	classifiers=[
-		 'Development Status :: 5 - Production/Stable',
-         'License :: OSI Approved :: MIT License',
-         'Intended Audience :: Developers',
-         'Natural Language :: English',
-         'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.5',
-         'Programming Language :: Python :: 3.6',
-         'Programming Language :: Python :: 3.7',
-         'Topic :: Internet',
-         'Topic :: Software Development :: Libraries',
-         'Topic :: Software Development :: Libraries :: Python Modules',
-         'Topic :: Utilities',
-	]
+	keywords='gmail email wrapper'
 )
