22541ac5705ae3e0f60624fe419fe342045dd0a8
mischievousdev <adevhubyt@gmail.com>
2020-02-16 11:39:56 +0530

    config done

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.split(os.path.dirname(
+    os.path.abspath(__file__)))[0])
 
 
 # -- Project information -----------------------------------------------------
@@ -31,15 +32,26 @@ release = '1.0.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.viewcode',
+	'sphinx.ext.githubpages',
+	'sphinx.ext.napoleon'
 ]
+
+source_suffix = '.rst'
+
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+pygments_style = None
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +59,25 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'style_external_links': True,
+    # Toc options
+    'navigation_depth': 4,
+    'titles_only': False
+}
+
+intersphinx_mapping = {
+    'python':
+        ('https://docs.python.org/3', None),
+    'requests':
+        ('http://docs.python-requests.org/en/master/', None),
+    'aiohttp':
+        ('https://aiohttp.readthedocs.io/en/stable/', None)
+}
